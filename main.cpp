15b3098e7ce011a1fd23efb1736828d392193baf
victor <vqiu25@gmail.com>
2024-06-24 17:37:53 +1200

    Feature: Add ball and paddles

--- main.cpp
+++ main.cpp
@@ -4,4 +4,26 @@
 
 using namespace std;
 
-int main() { return 0; }
+int main() {
+
+  const int screenWidth = 1280;
+  const int screenHeight = 800;
+  InitWindow(screenWidth, screenHeight, "Pong");
+  SetTargetFPS(60);
+
+  while (WindowShouldClose() == false) {
+    BeginDrawing();
+
+    // Pong Ball
+    DrawCircle(screenWidth / 2, screenHeight / 2, 20, WHITE);
+    // Paddles
+    DrawRectangle(10, screenHeight / 2 - (120 / 2), 25, 120, WHITE);
+    DrawRectangle(screenWidth - 25 - 10, screenHeight / 2 - (120 / 2), 25, 120,
+                  WHITE);
+
+    EndDrawing();
+  }
+
+  CloseWindow();
+  return 0;
+}
